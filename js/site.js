7c20b92e1e1c8927b9c5b35883b6305d76a049a1
EB03 <35344980+EB03@users.noreply.github.com>
2021-08-28 14:10:14 -0400

    Refactored html, js files

--- js/site.js
+++ js/site.js
@@ -4,42 +4,71 @@ function getString() {
     let fNum = document.getElementById("fizzNum").value;
     let bNum = document.getElementById("buzzNum").value;
 
-    getFizzBuzz(fNum, bNum);
+    fNum = parseInt(fNum);
+    bNum = parseInt(bNum);
 
-    // displayFizzBuzz(dFizzBuzz);
-}
-
-function getFizzBuzz(fNum, bNum) {
-    // let numbers = [];
-
-    // for (let i = sValue; i <= eValue; i++) {
-    //     numbers.push(i);
-    // }
-    // return numbers;
-
-    for (let i = 1; i <= 100; i++) {
-        // document.getElementById("results").innerHTML += `<p>${i}</p>`;
-
-        let results = document.getElementById("results")
-
-        if (i % fNum == 0 && i % bNum == 0) {
-            results.innerHTML += `<tr><td><img src="/img/FizzBuzz_Logo_Text.svg" alt="FizzBuzz_Logo_Text" width="90"></td></tr>`;
-        }
-
-        else if (i % bNum == 0) {
-            results.innerHTML += `<tr><td>Buzz</td></tr>`;
-        }
-
-        else if (i % fNum == 0) {
-            results.innerHTML += `<tr><td>Fizz</td></tr>`;
-        }
-
-        else {
-            results.innerHTML += `<tr><td>${i}</td></tr>`;
-        }
+    if (Number.isInteger(fNum) && Number.isInteger(bNum)) {
+        let dFizzBuzz = getFizzBuzz(fNum, bNum);
+        displayFizzBuzz(dFizzBuzz);
+    } else {
+        alert("You must enter integer values");
     }
 }
 
+function getFizzBuzz(fNum, bNum) {
+    let results = [];
+
+    for (let i = 1; i <= 100; i++) {
+
+        if (i % fNum == 0 && i % bNum == 0) {
+            results.push('FizzBuzz');
+        }
+
+        else if (i % bNum == 0) {
+            results.push('Buzz');
+        }
+
+        else if (i % fNum == 0) {
+            results.push('Fizz');
+        }
+
+        else {
+            results.push(i);
+        }
+    }
+
+    return results;
+}
+
 function displayFizzBuzz(dFizzBuzz) {
-    
+
+    let tableBody = document.getElementById("results");
+
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear the table
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < dFizzBuzz.length; i += 5) {
+
+        let tableRow = document.importNode(templateRow.content, true);
+
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].textContent = dFizzBuzz[i];
+        rowCols[0].classList = dFizzBuzz[i];
+
+        rowCols[1].textContent = dFizzBuzz[i+1];
+        rowCols[1].classList = dFizzBuzz[i+1];
+
+        rowCols[2].textContent = dFizzBuzz[i+2];
+        rowCols[2].classList = dFizzBuzz[i+2];
+
+        rowCols[3].textContent = dFizzBuzz[i+3];
+        rowCols[3].classList = dFizzBuzz[i+3];
+
+        rowCols[4].textContent = dFizzBuzz[i+4];
+        rowCols[4].classList = dFizzBuzz[i+4];
+
+        tableBody.appendChild(tableRow);
+    }
 }
